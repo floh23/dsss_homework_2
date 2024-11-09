610dd447c7aad0e4613fb8a796298016986c6c64
PaulundHasi <45520756+PaulundHasi@users.noreply.github.com>
2024-11-09 14:21:34 +0100

    renaming variables

--- math_quiz.py
+++ math_quiz.py
@@ -21,29 +21,29 @@ def function_C(n1, n2, o):
     return p, a
 
 def math_quiz():
-    s = 0
-    t_q = 3
+    start = 0
+    end = 3
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10)
-        n2 = function_A(1, 5)
-        o = function_B()
+    for iteration in range(end):
+        number1 = function_A(1, 10)
+        number2 = function_A(1, 5)
+        operator = function_B()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = function_C(number1, number2, operator)
         print(f"\nQuestion: {PROBLEM}")
         useranswer = input("Your answer: ")
         useranswer = int(useranswer)
 
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            s += -(-1)
+            start += -(-1)
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {start}/{end}")
 
 if __name__ == "__main__":
     math_quiz()
