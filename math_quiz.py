2a0b484094f8b0cfb303c14caeeb9b134f79f0aa
PaulundHasi <45520756+PaulundHasi@users.noreply.github.com>
2024-11-09 18:32:47 +0100

    adding comments

--- math_quiz.py
+++ math_quiz.py
@@ -4,21 +4,31 @@ import numpy as np
 
 def function_A(min, max):
     """
-    Random integer.
+    function returns a random integer between min and max
+    :return: Random integer.
     """
     return random.randint(min, max)
 
-
 def function_B():
+    """
+    function to return either addition, subtraction oder multiplication as char
+    :return: one random operator of three
+    """
     return random.choice(['+', '-', '*'])
 
-
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 + n2
-    elif o == '-': a = n1 - n2
-    else: a = n1 * n2
-    return p, a
+def function_C(number1, number2, operator):
+    """
+    calculates an operation with two numbers and an operator
+    :param number1: number 1 for given operation
+    :param number2: number 2 for given operation
+    :param operator: gives the operation that should be calculated
+    :return: function to be calculated,
+    """
+    product = f"{number1} {operator} {number2}"
+    if operator == '+': addedNumbers = number1 + number2
+    elif operator == '-': addedNumbers = number1 - number2
+    else: addedNumbers = number1 * number2
+    return product, addedNumbers
 
 def math_quiz():
     start = 0
@@ -28,12 +38,15 @@ def math_quiz():
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
     for iteration in range(end):
+        #create two random numbers and an operator
         number1 = function_A(1, 10)
         number2 = function_A(1, 5)
         operator = function_B()
 
+        #create the function and the calculated operation
         PROBLEM, ANSWER = function_C(number1, number2, operator)
         print(f"\nQuestion: {PROBLEM}")
+        #wait for user to answer the created operation
         useranswer = input("Your answer: ")
         useranswer = int(useranswer)
 
