1056fb094194af25c4765d82563af946e3ec01db
PaulundHasi <45520756+PaulundHasi@users.noreply.github.com>
2024-11-10 22:25:50 +0100

    fill setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='math_quiz',
+    version='0.1.0',
+    description='creating a math quiz',
+    url='https://github.com/floh23/dsss_homework_2.git',
+    author='Florian Hasreiter',
+    author_email='florian.hasreiter@fau.de'
+    )
